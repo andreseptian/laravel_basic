64addd6b9604a74bc818b73eb45232d346c08ec6
Andre Septian <andre.septian2016@student.uny.ac.id>
2020-09-26 20:03:13 +0700

    first commit

---
+++ resources/views/extra.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Coba-coba</title>
+</head>
+
+<body>
+    <h1>HAI !!!</h1>
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -2,17 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+//Route
+//Controller
+//View
+
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/artikel', 'ArticleController@index');
+Route::get('/artikel/1', 'ArticleController@show');
